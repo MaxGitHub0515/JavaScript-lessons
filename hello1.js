369625b7f47053a96064cc5c35723f83dfa12def
m.pavlovsky <pavloskymaxym4@gmail.com>
2022-07-03 09:53:36 +0300

    Main theory

---
+++ hello1.js
@@ -0,0 +1,21 @@
+// 5/27/
+console.log("I will do my best to become more clever")
+
+// Anything following double slashes is a comment
+// Read the comments carefully: they explain the JavaScript code
+
+// Variables are declared with the let keyword
+let x;  // declared a variable x
+// Values can be assigned to variables using an = sign
+ x = 0; // Now the variable x has the value 0
+x; // => 0; A variable evaluates to(до) its value
+
+// JS suppors several types of value
+x=1; // Numbers
+x=0,01; // Numbers can be integers or reals
+x="hello world"; // Strings
+x='hello world';// this is a string too
+x=true; // A Boolean value
+x=false; // The other Boolean value
+x=null; // like (nal). Null is a special value that means "no value"
+x=undefined; // Undefined is another special value like null
---
+++ hello2.js
@@ -0,0 +1,22 @@
+// 5/27/
+
+// JavaScript's most important datatype is the object
+// An object is a collection of name/ value pairs, or a string to value map 
+let book = {     // Objects are enclosed in curly braces       
+  topic:"JavaScript",  // The property "topic" has value "JavaScript"
+  edition:7 // The property "edition" has value 7
+}; // The curly brace marks the end of the object      
+// Access the properties of an object with . or []
+book.topic //=> "JavaScript"
+book["edition"]
+book.author = "Flanagan";
+book.contets = {} // {} is an empty object with no properties
+
+// Arrays 
+let primes = [2, 3, 5, 7]; // An array of 4 values , delimited with [ , ]
+primes[0] // => 2 :  the first element (index 0)
+
+primes.length // => 4 : how many elements in the array
+primes[primes.length-1] // => 7 : the last element of the array
+// it equals 7 because you primes.length equals 4 and then 4 subtracts 1 and  all these  will equal  3
+let empty = []; // empty Array
